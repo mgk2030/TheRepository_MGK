aa28b6eb7f47018799b1ed02a69c6303b60cb2ab
Girish Kumar Marpally <88535544+mgk2030@users.noreply.github.com>
2022-05-20 16:48:28 +0400

    Add files via upload

---
+++ Application/TVM_Calculator/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tvmcalculator;
+
+/**
+ *
+ * @author giris
+ */
+public class Main {
+    public static void main(String[] args){
+        TVM_FV tvnm_fv = new TVM_FV();
+        tvnm_fv.setVisible(true);
+    }
+}
