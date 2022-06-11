e170d8bf42f31dce1aff129bb4ff8f56b0139a5c
Girish Kumar Marpally <88535544+mgk2030@users.noreply.github.com>
2022-06-11 16:47:04 +0400

    Add files via upload

---
+++ Application/ASP.NET_E-Commerce_Website/dbo.utbl.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[utbl] (
+    [Id]                INT          NOT NULL,
+    [ProductName]       NCHAR (50)   NULL,
+    [QuantityAvailable] INT          NULL,
+    [Contact]           NCHAR (10)   NULL,
+    [ProductDetails]    NCHAR (1000) NULL,
+    [ProductID]         NCHAR (10)   NULL,
+    [Model]             NCHAR (50)   NULL,
+    [Make]              NCHAR (5)    NULL,
+    [Description]       NCHAR (1000) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
